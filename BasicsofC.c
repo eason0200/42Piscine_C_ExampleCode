fb45cdd7bfc9deec9f0283498dc98abff61f2a5f
Eason <easonapu@gmail.com>
2022-08-19 08:12:08 +0800

    Update BasicsofC.c

--- BasicsofC.c
+++ BasicsofC.c
@@ -1,8 +1,15 @@
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+	write(1,&c,1);	
+}
+
+
 int  main()
 {
-		write(1,", \n", 3);
-		return 0;
+	write(1,", \n", 3);
+	ft_putchar('test');
+	return 0;
 }
 
