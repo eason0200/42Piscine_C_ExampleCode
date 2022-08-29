0a955ebc16094f8b57b2f6edf2a89046c8571065
Eason <easonapu@gmail.com>
2022-08-29 19:20:49 +0800

    Add files via upload

---
+++ Rush01/main.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cping-xu <cping-xu@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 10:11:05 by cping-xu          #+#    #+#             */
+/*   Updated: 2022/08/28 17:48:07 by cping-xu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "function.h"
+
+int	initboard(int *input);
+
+int	checkrepeat(char *input)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	temp = 0;
+	while (input[i])
+	{
+		if (input[i] == '1')
+			temp++;
+		i++;
+	}
+	if (temp != 4)
+		return (0);
+	return (1);
+}
+
+int	checkinput(char *arg, int *input)
+{
+	int	i;
+	int	out;
+
+	i = 0;
+	out = 0;
+	if (checkrepeat(arg) == 1)
+	{
+		while (arg[i])
+		{
+			if ((arg[i] >= '1' && arg[i] <= '4') && ((arg[i + 1] == ' ')
+					|| ((arg[i + 1] == 0) && (arg[i - 1] == ' '))))
+			{
+				input[out] = (arg[i] - '0');
+				out++;
+				i++;
+			}
+			if ((arg[i] >= 9 && arg[i] <= 13) || arg[i] == 32)
+				i++;
+			else
+				return (out);
+		}
+		input[out] = '\0';
+		return (out);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	input[16];
+
+	if (argc != 2)
+	{
+		ft_putstr("ERROR\n");
+	}
+	else
+	{
+		if (checkinput(argv[1], input) == 16)
+			initboard(input);
+		else
+			ft_putstr("ERROR\n");
+	}
+	return (0);
+}
